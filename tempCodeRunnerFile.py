d6803355b88d2f4d2e0360a5a48ff5d5497ffb77
CHG1922 <petrupetrusha@gmail.com>
2024-07-12 15:51:05 +0300

    ver.1

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,9 @@
+def index(rezhor=int(input()),rezvert=int(input())):
+#     indexhor=float(1920/rezhor)
+#     indexver=float(1080/rezvert)
+
+# def rescale(function):
+#     width=int(frame.shape[1]*scale)
+#     height=int(frame.shape[0]*scale)
+#     dimens=(width,height)
+#     return cv.resize(frame,dimens)
